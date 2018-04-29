c32bc6f38942badd804dbe7225017655f130ef4c
Tabatha Acosta <tabeli@MacBook-Pro-de-Tabatha.local>
2018-04-29 18:45:59 -0500

    Back del login

--- Back/app.js
+++ Back/app.js
@@ -1,20 +1,23 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var Sequelize = require('sequelize');
+var express = require('express'); //Librería que te permite montar el server en js
+var bodyParser = require('body-parser'); //Librería que te permite parsear datos a json
+var Sequelize = require('sequelize'); //Permite hacer una conexión a la base de datos
+var bcrypt = require('bcryptjs'); //Para los tokens
 
+//PArametros para conexion con base de datos
 var database = 'proyectoBases';
 var user = 'root';
 var password = 'mysql';
 var host = 'localhost';
 var dbType = 'mysql';
 
-var conection = new Sequelize(database, user, password,{
+//Creacion de instancia que se va a conectar a la base de datos
+var sequelize = new Sequelize(database, user, password,{
     host: host,
-    dialect: dbType
-
+    dialect: dbType    
 })
 
-conection
+// COnexion a la base de datos
+sequelize
 .authenticate()
 .then(() => {
     console.log('Conexion establecida exitosamente');
@@ -22,15 +25,37 @@ conection
     console.error('No se pudo realizar la conexión', err);
 });
 
+//Instancia del server
 var app = express();
+//Configura el server para que pueda parsear json's
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+//Definicion de rutas
 app.get('/', function(req, res){
-    return conection.query("select * from productos").spread((results, metadata) => {
+    return sequelize.query("select * from productos").spread((results, metadata) => {
         res.json(results);
     })
 })
 
+app.post('/validate',function(req, res){
+    return sequelize.query("select * from clientes where email = \"" + req.body.mail + "\"",{type: sequelize.QueryTypes.SELECT})
+    .then(user => {
+        return sequelize.query("select pwd from clientes where id = " + user[0].id ,{type: sequelize.QueryTypes.SELECT})
+        .then(pass => {
+            if(pass[0].pwd == req.body.pwd){
+                var salt = bcrypt.genSaltSync(user[0].id);
+                res.json({"token": salt});
+            }
+            else{
+                res.json({"error": "Password does not match"})
+            }
+            
+        })
+        .catch(error => res.status(400).send(error))
+    })
+    .catch(error => res.status(400).send(error))
+})
 
 app.listen(8080);
 console.log('Escuchando en puerto 8080')
